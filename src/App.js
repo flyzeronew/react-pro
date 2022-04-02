93258dd5412126c51c2af54b955670d78fa2bb76
flyzero <flyzeronew@gmail.com>
2022-04-02 09:50:43 +0800

    推

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import Head from './Head';
 import Header from './header';
+import {Helmet} from "react-helmet";
 import Myjs from './Myjs';
 import './css/program_master.css';
 import './css/program_article_share.css';
@@ -8,8 +8,15 @@ function App() {
   
   return (
     <div className="program_container">      
-      <Head/> 
       <Myjs/>
+      <Helmet>
+                <meta charSet="utf-8" />
+                <title>節目公版</title>
+                <meta name="viewport" content="width=device-width"/>
+                <meta name="keywords" content="關鍵字" />
+                <meta name="description" content="導言"/>
+                
+            </Helmet> 
       <header>
         <Header/>
       </header>
