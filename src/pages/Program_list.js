3f3b40f2a6f888c391ac1353b28acaa22b114584
flyzero <flyzeronew@gmail.com>
2022-08-10 19:56:43 +0800

    頁面切換測試

--- src/pages/Program_list.js
+++ src/pages/Program_list.js
@@ -13,6 +13,7 @@ import './../css/program_article_share.css';
 import queryString from "query-string";
 import Footer from './../components/Footer.js';
 import { DFPSlotsProvider, AdSlot } from 'react-dfp';
+import ReactPaginate from 'react-paginate';
 
 var get_pathname=window.location.pathname.split('/').filter(Boolean);
 var get_id=get_pathname[0];
@@ -20,6 +21,7 @@ var get_id=get_pathname[0];
 let img = 'img';
 let ad_img = 'ad_img';
 let logo = 'logo';
+
 
 function Program_list() {
     const [loading, setLoading] = useState('')
@@ -29,6 +31,9 @@ function Program_list() {
     const [program_info, setProgram_info] = useState('')
     const [relative_news, setRelative_news] = useState('')
     const [articles, setArticles] = useState('')
+    const [pageCount, setPageCount] = useState(0)
+    const [offset, setOffset] = useState(0)
+    const [perPage] = useState(10);
     
     //多組api fetch
     const urls = ['https://tvbsapp.tvbs.com.tw/program_api/index_cover', "https://tvbsapp.tvbs.com.tw/program_api/social","https://tvbsapp.tvbs.com.tw/program_api/broadcast_time","https://tvbsapp.tvbs.com.tw/program_api/program_info","https://tvbsapp.tvbs.com.tw/program_api/related_news_by_keywords"];
@@ -46,26 +51,29 @@ function Program_list() {
       setRelative_news(result5.data.slice(0,2))
     };
 
-    const urls2 =["https://tvbsapp.tvbs.com.tw/program_api/wonderful_list"];
+    const urls2 =["https://tvbsapp.tvbs.com.tw/program_api/wonderful_list","https://tvbsapp.tvbs.com.tw/program_api/wonderful_pages"];
     const getDataFromServer2 = async () => {
       setLoading(true);
-      const [result1] = await Promise.all(
-        urls2.map((url) => fetch(url+"?id="+ get_id +"&limit=12&page=1").then((res) => res.json()))
+         const [result1,result2] = await Promise.all(
+        
+        urls2.map((url) => fetch(url+"?id="+ get_id +"&limit=12&page="+offset).then((res) => res.json()))
      );
       setLoading(false);
       setArticles(result1.data);
-
-      console.log(articles);
+      setPageCount(result2.data.length)
+      
     };
 
-
-
-
+const handlePageClick = (e) => {
+  const selectedPage = e.selected;
+  setOffset(selectedPage + 1)
+};
+console.log(handlePageClick);
     // 模擬componentDidMount
     useEffect(() => {
         getDataFromServer()
         getDataFromServer2()
-    }, [])
+    }, [offset])
 
     //fb iframe
     let fb_url=social.facebook;
@@ -81,7 +89,7 @@ function Program_list() {
 
   return (
     <div className="program_container">
-
+       
      <div id="back">
         <div id="back-img1"><img src={gotop} alt={ad_img}/></div>
      </div>
@@ -140,19 +148,19 @@ function Program_list() {
                   </div>
 
                   <div className="jump_list pc_display">
-                    <ul>                   
-                        <li><a href="#">上一頁</a></li>
-                        <li className="act"><a href="#">1</a></li>
-                        <li><a href="#">2</a></li>
-                        <li><a href="#">3</a></li>
-                        <li><a href="#">4</a></li>
-                        <li><a href="#">5</a></li>
-                        <li><a href="#">6</a></li>
-                        <li><a href="#">7</a></li>
-                        <li><a href="#">下一頁</a></li>
-
-                     </ul>
-                  </div>
+                    <ReactPaginate
+                      previousLabel={"上一頁"}
+                      nextLabel={"下一頁"}
+                      breakLabel={"..."}
+                      marginPagesDisplayed={1}
+                      pageRangeDisplayed={5}
+                      containerClassName={"jump_list"}
+                      activeClassName={"act"}
+                      pageCount={pageCount}
+                      onPageChange={handlePageClick}
+                    />
+                      
+                    </div>
               </div>
           </div>
 
@@ -161,7 +169,7 @@ function Program_list() {
             <div className="program_content_right_time"><p className="font16_3">{time.content}</p></div>
             <div className="program_content_right_ad_box">
              
-              <DFPSlotsProvider dfpNetworkId={'21697024903'} adUnit="news.tvbs.com.tw_pc_index_list1">
+              <DFPSlotsProvider dfpNetworkId={'21697024903'} adUnit="news.tvbs.com.tw_pc_read_r1">
               <AdSlot sizes={[[300,250]]} />
               </DFPSlotsProvider>
            
@@ -172,8 +180,8 @@ function Program_list() {
 
             <div className="program_content_right_ad_box">
              
-             <DFPSlotsProvider dfpNetworkId={'21697024903'} adUnit="news.tvbs.com.tw_pc_index_list2">
-             <AdSlot sizes={[[300,250],[300,600]]} />
+             <DFPSlotsProvider dfpNetworkId={'21697024903'} adUnit="news.tvbs.com.tw_pc_read_r2">
+             <AdSlot sizes={[[300,600]]} />
              </DFPSlotsProvider>
           
            </div>
